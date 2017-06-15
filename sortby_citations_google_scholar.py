e09077a86d291e3a762a2db6d3a8f39a5c59a547
Álvaro Brey <alvaro.brv@gmail.com>
2017-06-15 14:07:12 +0200

    sortby_citations_google_scholar: Reformat file

--- sortby_citations_google_scholar.py
+++ sortby_citations_google_scholar.py
@@ -23,36 +23,39 @@ import pandas as pd
 
 def get_citations(content):
     out = 0
-    for char in range(0,len(content)):
-        if content[char:char+9] == 'Cited by ':
-            init = char+9                          
-            for end in range(init+1,init+6):
+    for char in range(0, len(content)):
+        if content[char:char + 9] == 'Cited by ':
+            init = char + 9
+            for end in range(init + 1, init + 6):
                 if content[end] == '<':
                     break
             out = content[init:end]
     return int(out)
-    
+
+
 def get_year(content):
-    for char in range(0,len(content)):
+    for char in range(0, len(content)):
         if content[char] == '-':
-            out = content[char-5:char-1]
+            out = content[char - 5:char - 1]
     if not out.isdigit():
         out = 0
     return int(out)
 
+
 def get_author(content):
-    for char in range(0,len(content)):
+    for char in range(0, len(content)):
         if content[char] == '-':
-            out = content[2:char-1]
+            out = content[2:char - 1]
             break
     return out
 
+
 # Update these variables according to your requirement
-keyword = "'non intrusive load monitoring'" # the double quote will look for the exact keyword,
-                                            # the simple quote will also look for similar keywords
-number_of_results = 100 # number of results to look for on Google Scholar
-save_database = False # choose if you would like to save the database to .csv
-path = 'C:/_wittmann/nilm_100_exact_author.csv' # path to save the data
+keyword = "'non intrusive load monitoring'"  # the double quote will look for the exact keyword,
+# the simple quote will also look for similar keywords
+number_of_results = 100  # number of results to look for on Google Scholar
+save_database = False  # choose if you would like to save the database to .csv
+path = '/tmp/foo.csv'  # path to save the data
 
 # Start new session
 session = requests.Session()
@@ -64,38 +67,39 @@ citations = list()
 year = list()
 rank = list()
 author = list()
-rank.append(0) # initialization necessary for incremental purposes
+rank.append(0)  # initialization necessary for incremental purposes
 
 # Get content from 1000 URLs
-for n in range(0, number_of_results, 10):    
-    url = 'https://scholar.google.com/scholar?start='+str(n)+'&q='+keyword.replace(' ','+')
+for n in range(0, number_of_results, 10):
+    url = 'https://scholar.google.com/scholar?start=' + str(n) + '&q=' + keyword.replace(' ', '+')
     page = session.get(url)
     c = page.content
-    
+
     # Create parser
     soup = BeautifulSoup(c, 'html.parser')
-    
+
     # Get stuff
-    mydivs = soup.findAll("div", { "class" : "gs_r" })
-    
+    mydivs = soup.findAll("div", {"class": "gs_r"})
+
     for div in mydivs:
         try:
             links.append(div.find('h3').find('a').get('href'))
-        except: # catch *all* exceptions
-            links.append('Look manually at: https://scholar.google.com/scholar?start='+str(n)+'&q=non+intrusive+load+monitoring')
-        
+        except:  # catch *all* exceptions
+            links.append('Look manually at: https://scholar.google.com/scholar?start=' + str(
+                n) + '&q=non+intrusive+load+monitoring')
+
         try:
             title.append(div.find('h3').find('a').text)
-        except: 
+        except:
             title.append('Could not catch title')
-            
+
         citations.append(get_citations(str(div.format_string)))
-        year.append(get_year(div.find('div',{'class' : 'gs_a'}).text))
-        author.append(get_author(div.find('div',{'class' : 'gs_a'}).text))
-        rank.append(rank[-1]+1)
+        year.append(get_year(div.find('div', {'class': 'gs_a'}).text))
+        author.append(get_author(div.find('div', {'class': 'gs_a'}).text))
+        rank.append(rank[-1] + 1)
 
 # Create a dataset and sort by the number of citations
-data = pd.DataFrame(zip(author, title, citations, year, links), index = rank[1:], 
+data = pd.DataFrame(zip(author, title, citations, year, links), index=rank[1:],
                     columns=['Author', 'Title', 'Citations', 'Year', 'Source'])
 data.index.name = 'Rank'
 
@@ -103,12 +107,11 @@ data_ranked = data.sort(columns='Citations', ascending=False)
 print data_ranked
 
 # Plot by citation number
-plt.plot(rank[1:],citations,'*')
+plt.plot(rank[1:], citations, '*')
 plt.ylabel('Number of Citations')
 plt.xlabel('Rank of the keyword on Google Scholar')
-plt.title('Keyword: '+keyword)
+plt.title('Keyword: ' + keyword)
 
 # Save results
 if save_database:
-    data_ranked.to_csv(path, encoding='utf-8') # Change the path
-        
+    data_ranked.to_csv(path, encoding='utf-8')  # Change the path
